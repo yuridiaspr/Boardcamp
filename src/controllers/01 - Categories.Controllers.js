220fa35ad4a14007405118b53e77e6847752f4f4
Yuri Dias <yuridiaspr@gmail.com>
2022-12-11 17:03:57 -0300

    feat: get and post from categories route

--- src/controllers/01 - Categories.Controllers.js
+++ src/controllers/01 - Categories.Controllers.js
@@ -4,11 +4,20 @@ export async function insertCategory(req, res) {
   const { name } = req.body;
 
   try {
-    await connectionDB.query("INSERT INTO categories name VALUE $1;", [name]);
+    await connectionDB.query("INSERT INTO categories (name) VALUES ($1);", [
+      name,
+    ]);
     res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
   }
 }
 
-export async function findAllCategories(req, res) {}
+export async function findAllCategories(req, res) {
+  try {
+    const { rows } = await connectionDB.query("SELECT * FROM categories");
+    res.send(rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
